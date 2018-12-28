edf4043aab5717daec57b05b138fb85bafb3dbbc
Soulyana Lakew <soulyana.lakew@accenturefederal.com>
2018-12-28 09:53:51 -0500

    Timers

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,3 +3,18 @@ import { Car } from './models/car';
 
 let car = new Car(123);
 console.log(car.id);
+
+//Timers - fire off aysnchorously 
+//setTimeout() executes a function one time in the future
+
+let timeoutId = setTimeout( function() {
+    console.log('1 second passed');
+}, 1000);
+//setInterval() fires repeatedly 
+
+let intervalId = setInterval( function() {
+    console.log('1 second passed');
+}, 1000);
+
+//if need to cancel
+clearInterval(intervalId);
