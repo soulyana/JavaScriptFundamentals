c5d079c5f8587cb1f227bf7b70e882b56e8ecd0d
Soulyana Lakew <soulyana.lakew@accenturefederal.com>
2018-12-28 18:15:28 -0500

    Accessing Form Fields

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -127,8 +127,11 @@ try {
 let form = document.getElementById('user-form');
 
 form.addEventListener('submit', event => {
+    let user = form.elements['user'];
+    let avatarFile = form.elements['avatar-file'];
 
+    console.log(user.value, avatarFile.value);
     //prevent the browser from submitting the form
-    //event.preventDefault();
+    event.preventDefault();
 });
 
